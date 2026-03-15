0feab6c3d228987ec7e66eba8f1ee5468eda2edb
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-03-15 17:23:27 +0100

    add task 3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,22 @@
-import {files} from './data.js';
+import { files } from './data.js';
 
+const convertToBytes = (length, unit = 'B') => {
+	const units = {
+		B: 1,
+		KB: 1024,
+		MB: 1024 ** 2,
+		GB: 1024 ** 3,
+	};
+
+	return length * (units[unit] || 1);
+};
+
+const getFileSizeInBytes = ({ size: { length, unit = 'B' } }) => {
+	return convertToBytes(length, unit);
+};
+
+const getTotalSize = files => {
+	return files.map(file => getFileSizeInBytes(file)).reduce((sum, value) => sum + value, 0);
+};
+
+console.log(getTotalSize(files));
