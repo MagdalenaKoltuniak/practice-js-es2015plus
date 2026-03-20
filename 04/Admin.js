9a81f16339d5b797ff38ce5fceff624577371699
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-03-20 10:15:25 +0100

    refactor: remove unnecessary constructor from Admin class

--- 04/Admin.js
+++ 04/Admin.js
@@ -1,10 +1,6 @@
 import User from './User.js';
 
 export default class Admin extends User {
-	constructor(data) {
-		super(data);
-	}
-
 	isPasswordCorrect() {
 		return this.password.length >= 10;
 	}
